275903867c1fdc3add24d2161135bafb11714acc
Adrieli Martins <martinsadrieli501@gmail.com>
2026-09-10 17:24:21 -0300

    feat: adiciona diagnóstico do servidor com módulo OS

--- aula01-revisao-nodejs-npm/diagnostico.js
+++ aula01-revisao-nodejs-npm/diagnostico.js
@@ -1,16 +1,17 @@
-//! importando o modulo 'os' do Node.js para obter informações sobre servidor
+//! importando o modulo 'os' do Node.js para obter informacoes sobre servidor
 const os = require('os');
 
-//! Exibindo informações sobre o sistema operacional e hardware
+//! exibindo informacoes sobre o sistema operacional e hardware do servidor
 console.log('=== DIAGNÓSTICO DO SERVIDOR ===\n');
 
-const plataforma = os.platform(); //! obtendo a plataforma do sistema operacional
-const memoriaTotal = (os.totalmem() / (1024 ** 3)).toFixed(2);
-const memoriaLivre = (os.freemem() / (1024 ** 3)).toFixed(2);
-const cpus = os.cpus(); //! obtendo informações sobre os processamento
+
+const plataforma = os.platform(); //! obtendo a plataforma do sistema operacional 
+const memoriaTotal = (os.totalmem() / (1024 ** 3).toFixed(2)); 
+const memoriaLivre = (os.freemem() / (1024 ** 3).toFixed(2));
+const cpus = os.cpus(); //! obtendo informacoes sobre os processamentos 
 
 console.log(`Arquitetura OS: ${plataforma}`);
 console.log(`Memoria RAM Total: ${memoriaTotal} GB`);
-console.log(`Memória RAM Livre: ${memoriaLivre} GB`);
+console.log(`Memoria RAM Livre: ${memoriaLivre} GB`);
 console.log(`Cores da CPU: ${cpus.length}`);
 console.log(`Processador: ${cpus[0].model}`);
